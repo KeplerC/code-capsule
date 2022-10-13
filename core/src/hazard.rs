2d7cc09d11f31b983e2ef82659f14aa9d06d1db1
Eric Chen <kych@berkeley.edu>
2022-10-12 17:18:35 -0700

    get things started

--- core/src/hazard.rs
+++ core/src/hazard.rs
@@ -1,4 +1,13 @@
 use utils::error::Result;
+extern crate libloading as lib;
+
+fn call_dynamic() -> lib::Result<u32> {
+    let lib = lib::Library::new("/path/to/liblibrary.so")?;
+    unsafe {
+        let func: lib::Symbol<unsafe extern fn() -> u32> = lib.get(b"my_func")?;
+        Ok(func())
+    }
+}
 
 /// Return, randomly, true or false
 pub fn generate_hazard() -> Result<bool> {
