5cdb13e537134a0f1f7b1d664e8835a72ae0aff6
Eric Chen <kych@berkeley.edu>
2022-10-12 17:26:16 -0700

    loading the .so works

--- core/src/hazard.rs
+++ core/src/hazard.rs
@@ -2,14 +2,15 @@ use utils::error::Result;
 extern crate libloading as lib;
 
 fn call_dynamic() -> lib::Result<u32> {
-    let lib = lib::Library::new("/path/to/liblibrary.so")?;
+    let lib = lib::Library::new("/home/gdpmobile8/codecapsule/example_lib/lib_worker.so")?;
     unsafe {
-        let func: lib::Symbol<unsafe extern fn() -> u32> = lib.get(b"my_func")?;
+        let func: lib::Symbol<unsafe extern fn() -> u32> = lib.get(b"run_worker")?;
         Ok(func())
     }
 }
 
 /// Return, randomly, true or false
 pub fn generate_hazard() -> Result<bool> {
+    call_dynamic();
     Ok(rand::random())
 }
---
+++ example_lib/library.c
@@ -0,0 +1,8 @@
+
+
+#include "stdio.h"
+
+int run_worker(){
+    printf("HERE IS THE WORKER!!!\n");
+    return 0;
+}
