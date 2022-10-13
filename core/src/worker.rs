1d2f8b73a9aac182185bcbcf2fe822326e3bbb3e
Eric Chen <kych@berkeley.edu>
2022-10-12 19:04:58 -0700

    emulate the loading with fs

--- core/src/worker.rs
+++ core/src/worker.rs
@@ -1,8 +1,11 @@
 use utils::error::Result;
+use std::fs;
 extern crate libloading as lib;
 
 fn load_module(lib_name:&str, dst_lib_path:String) -> Result<String> {
+    let oracle_lib_path: String = "/home/gdpmobile8/codecapsule/example_lib/".to_owned();
 
+    fs::copy(oracle_lib_path + lib_name, dst_lib_path.clone() + lib_name)?;
     Ok(dst_lib_path + lib_name)
 }
 
@@ -26,16 +29,17 @@ fn call_worker_module(dyn_path:String) -> Result<bool> {
 /// Return, randomly, true or false
 pub fn run_worker() -> Result<bool> {
 
-    let random_lib_loader: bool = rand::random::<bool>();
-    let lib_dir_path: String = "/home/gdpmobile8/codecapsule/example_lib/".to_owned();
+    let random_lib: bool = rand::random::<bool>();
+    // let dst_lib_path: String = "/home/gdpmobile8/codecapsule/example_lib/".to_owned();
+    let dst_lib_path: String = "/tmp/".to_owned();
     let lib1_name: &str = "lib_worker_1.so";
     let lib2_name: &str = "lib_worker_2.so";
     let dyn_path:String;
 
-    if random_lib_loader {
-        dyn_path = load_module(lib1_name, lib_dir_path)?;
+    if random_lib {
+        dyn_path = load_module(lib1_name, dst_lib_path)?;
     } else {
-        dyn_path = load_module(lib2_name, lib_dir_path)?;
+        dyn_path = load_module(lib2_name, dst_lib_path)?;
     }
 
     call_worker_module(dyn_path)
